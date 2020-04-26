c9924fdd9fdfa8d5d9504cfe19fc1d9364a5bd31
frinzekt <frinzekt@gmail.com>
2020-04-27 00:27:58 +0800

    user and privileges model

--- app/models.py
+++ app/models.py
@@ -14,3 +14,50 @@ class Base(db.Model):
 class BaseAutoPrimary(Base):
     __abstract__ = True
     id            = db.Column(db.Integer, primary_key=True)
+
+# Define a User model
+class User(BaseAutoPrimary):
+
+    __tablename__ = 'user'
+
+    # User Name
+    username    = db.Column(db.String(128), index=True, unique=True)
+
+    # Identification Data: email & password
+    email    = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(192),  nullable=False)
+
+    # Authorisation Data: role & status
+    # Null Role refers to normal user
+    role = db.Column(db.Integer, db.ForeignKey("privileges.id"))
+
+
+    #Password Methods
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # New instance instantiation procedure
+    def __init__(self, username, email, password):
+
+        self.username     = username
+        self.email    = email
+        self.password_hash = generate_password_hash(password)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+class Privileges(BaseAutoPrimary):
+    __tablename__ = "privileges"
+
+    #privilege name
+    name =db.Column(db.String(128), index=True, unique=True)
+
+    # One To Many Relationship
+    user = db.relationship("User",backref="users",lazy="dynamic")
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Privileges %r>' % (self.name)
+
