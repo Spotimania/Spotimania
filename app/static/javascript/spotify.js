c8ca5abf46bbacc5123dc25692efcdb04315b225
Swarup <syrupson8@gmail.com>
2020-05-05 18:08:48 +0800

    Revert "Revert "added search by track""
    This reverts commit 732ad2cd00bfcc200d0277e4049ace72065ce611.

--- app/static/javascript/spotify.js
+++ app/static/javascript/spotify.js
@@ -16,14 +16,14 @@ $.ajax({
 
 
 window.onload=function(){
-var el = document.getElementById('submit');
+var el = document.getElementById('submitArtist');
 if (el){
-document.querySelector("#submit").addEventListener("click", function(event) {
-	document.getElementById('artists').innerHTML=('')
+document.querySelector("#submitArtist").addEventListener("click", function(event) {
+	document.getElementById('results').innerHTML=('')
 	event.preventDefault();
 	//ajax call to get data for artist
 	$.ajax({
-          url: "https://api.spotify.com/v1/search?q=" + $('#tracks').val() + '&type=artist',
+          url: "https://api.spotify.com/v1/search?q=" + $('#artist').val() + '&type=artist',
           type: 'GET',
           contentType: 'application/json',
           headers: {
@@ -32,7 +32,7 @@ document.querySelector("#submit").addEventListener("click", function(event) {
           success: function(data) {
 	            
 	            for (i=0;i<data.artists.items.length;i++) {
-	            	document.getElementById('artists').innerHTML+=("<li><button class='artistname' type='button'>"+(data.artists.items[i].name)+"</button></li>");
+	            	document.getElementById('results').innerHTML+=("<li><button class='artistname' type='button'>"+(data.artists.items[i].name)+"</button></li>");
 	            }
 	            $('.artistname').click(function(event) {
 	            	document.getElementById('artists').innerHTML=('')
@@ -54,7 +54,7 @@ document.querySelector("#submit").addEventListener("click", function(event) {
                  				},
                  			success: function(data) {
                  				for (i=0;i<data.tracks.length;i++) {
-	            					document.getElementById('artists').innerHTML+=("<li><input class='tracks' type='submit' value='"+(data.tracks[i].name)+"'></li>");
+	            					document.getElementById('results').innerHTML+=("<li><input class='tracks' type='submit' value='"+(data.tracks[i].name)+"'></li>");
 	            				}
 	            				$('.tracks').click(function(event) {
 	            					document.getElementById('tracks').innerHTML=('')
@@ -112,5 +112,64 @@ document.querySelector("#submit").addEventListener("click", function(event) {
           
             });
         });
+	}
+
+
+	var el = document.getElementById('submitTrack');
+if (el){
+	document.querySelector("#submitTrack").addEventListener("click", function(event) {
+		document.getElementById('results').innerHTML=('')
+		event.preventDefault();
+		//ajax call to get data for artist
+		$.ajax({
+	          url: "https://api.spotify.com/v1/search?q=" + $('#track').val() + '&type=track',
+	          type: 'GET',
+	          contentType: 'application/json',
+	          headers: {
+	                    "Authorization": "Bearer " + accessToken
+	                 },
+	          success: function(data) {
+	          			console.log(data)
+	          			for (i=0;i<data.tracks.items.length;i++) {
+			            	document.getElementById('results').innerHTML+=("<li><button class='songname' type='button'>"+(data.tracks.items[i].name)+"</button></li>");
+			            }
+			            $('.songname').click(function(event) {
+					            	var target = event.target || event.srcElement;
+					            	var name = target.innerHTML;
+					            	console.log(name);
+					            	for (i=0;i<data.tracks.items.length;i++){
+  											//ajax call to get data for specific track
+  											if (name == data.tracks.items[i].name) {
+					                 				$.ajax({
+							  							url: "/playlist",
+							  							type: "POST",
+							  							contentType: "application/json",
+							  							dataType: 'json',
+							  							data: JSON.stringify({
+							  								songID: data.tracks.items[i].id,
+							  								artist: data.tracks.items[i].album.artists[0].name,
+							  								songName: data.tracks.items[i].name,
+							  								prevIMG: data.tracks.items[i].album.images[0].url,
+							  								prevURL: data.tracks.items[i].preview_url,
+							  								album: data.tracks.items[i].album.name,
+
+							  							})
+
+					                 					});
+					                 			}
+					                 		}
+				                 			});
+  										}
+  									
+	            				});
+	            	
+                 				
+	          		})
+	        	
+			
+		}
+
+
 }
-}
+
+
