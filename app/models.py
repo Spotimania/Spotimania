39564b9fb6eef9e4bfcdae63f71c823b0f65f558
frinzekt <frinzekt@gmail.com>
2020-05-14 11:30:06 +0800

    convert object to dict (for JSON)

--- app/models.py
+++ app/models.py
@@ -3,19 +3,35 @@ from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+# Checking Types
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+import types
+
 # Define a base model for other database tables to inherit
 # Useful for logging purposes
+
+
 class Base(db.Model):
 
-    __abstract__  = True
+    __abstract__ = True
 
-    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    date_created = db.Column(db.DateTime,  default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
-                                           onupdate=db.func.current_timestamp())
+                              onupdate=db.func.current_timestamp())
+
+    def to_dict(self):
+        class_vars = vars(type(self))  # get any "default" attrs defined at the class level
+        inst_vars = vars(self)  # get any attrs defined on the instance (self)
+        all_vars = dict(class_vars)
+        all_vars.update(inst_vars)
+        # filter out private attributes
+        public_vars = {k: v for k, v in all_vars.items() if not (k.startswith('_') or (isinstance(v, types.FunctionType)) or (isinstance(v,InstrumentedAttribute)))}
+        return public_vars
+
 
 class BaseAutoPrimary(Base):
     __abstract__ = True
-    id            = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
 
 # Login User Loader / Session Retriever
 @login.user_loader
@@ -23,46 +39,50 @@ def load_user(id):
     return User.query.get(int(id))
 
 # Define a User model
-class User(UserMixin,BaseAutoPrimary):
+
+
+class User(UserMixin, BaseAutoPrimary):
 
     __tablename__ = 'user'
 
     # User Name
-    username    = db.Column(db.String(128), index=True, unique=True)
+    username = db.Column(db.String(128), index=True, unique=True)
 
     # Identification Data: email & password
-    email    = db.Column(db.String(128), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(192),  nullable=False)
 
     # Authorisation Data: role & status
     # Null Role refers to normal user
     privilegeId = db.Column(db.Integer, db.ForeignKey("privileges.id"))
 
-    #Back Reference
-    userPlayedPlaylist = db.relationship("Results", backref="user", lazy="dynamic")
+    # Back Reference
+    userPlayedPlaylist = db.relationship(
+        "Results", backref="user", lazy="dynamic")
 
-    #Password Methods
+    # Password Methods
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
     # New instance instantiation procedure
-    def __init__(self, username, email, password,role=None):
-        self.username     = username
-        self.email    = email
+    def __init__(self, username, email, password, role=None):
+        self.username = username
+        self.email = email
         self.password_hash = generate_password_hash(password)
-        self.role=role
+        self.role = role
 
     def __repr__(self):
         return '<User %r>' % (self.username)
 
+
 class Privileges(BaseAutoPrimary):
     __tablename__ = "privileges"
 
-    #privilege name
-    name =db.Column(db.String(128), index=True, unique=True)
+    # privilege name
+    name = db.Column(db.String(128), index=True, unique=True)
 
     # One To Many Relationship
-    user = db.relationship("User",backref="role",lazy="dynamic")
+    user = db.relationship("User", backref="role", lazy="dynamic")
 
     def __init__(self, name):
         self.name = name
@@ -71,17 +91,21 @@ class Privileges(BaseAutoPrimary):
         return '<Privileges %r>' % (self.name)
 
 # Playlist model with track data
+
+
 class Song(BaseAutoPrimary):
-    spotifySongID = db.Column(db.String(100),unique=True)
+    spotifySongID = db.Column(db.String(100), unique=True)
     prevURL = db.Column(db.String(100))
     prevIMG = db.Column(db.String(100))
     songName = db.Column(db.String(100))
     artist = db.Column(db.String(100))
     album = db.Column(db.String(100))
 
-    #Back Reference
-    songsInPlaylist = db.relationship("Playlist_Song", backref="song", lazy="dynamic")
-    songsInResults = db.relationship("IndividualResults", backref="song", lazy="dynamic")
+    # Back Reference
+    songsInPlaylist = db.relationship(
+        "Playlist_Song", backref="song", lazy="dynamic")
+    songsInResults = db.relationship(
+        "IndividualResults", backref="song", lazy="dynamic")
 
     def __init__(self, spotifySongID, prevURL, prevIMG, songName, artist, album):
         self.spotifySongID = spotifySongID
@@ -94,6 +118,7 @@ class Song(BaseAutoPrimary):
     def __repr__(self):
         return '<Song %r>' % (self.songName)
 
+
 class Playlist_Song(BaseAutoPrimary):
     __tablename__ = "playlist_song"
     playlistId = db.Column(db.Integer, db.ForeignKey("playlist.id"))
@@ -104,20 +129,24 @@ class Playlist_Song(BaseAutoPrimary):
         self.song = song
 
     def __repr__(self):
-        return '<Song %r in Playlist %r>' % (self.song.songName,self.playlist.playlistName)
+        return '<Song %r in Playlist %r>' % (self.song.songName, self.playlist.playlistName)
+
 
 class Playlist(BaseAutoPrimary):
     playlistName = db.Column(db.String(100))
 
-    #Back Reference
-    playlistInSong = db.relationship("Playlist_Song", backref="playlist", lazy="dynamic")
-    playlistUser = db.relationship("Results", backref="playlist", lazy="dynamic")
+    # Back Reference
+    playlistInSong = db.relationship(
+        "Playlist_Song", backref="playlist", lazy="dynamic")
+    playlistUser = db.relationship(
+        "Results", backref="playlist", lazy="dynamic")
 
-    def __init__(self,name):
+    def __init__(self, name):
         self.playlistName = name
 
     def __repr__(self):
         return '<Playlist %r>' % (self.playlistName)
+
 
 
 class Results(BaseAutoPrimary):
@@ -125,27 +154,29 @@ class Results(BaseAutoPrimary):
     userId = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     # Back Reference
-    resultsIndividual = db.relationship("IndividualResults", backref="result", lazy="dynamic")
+    resultsIndividual = db.relationship(
+        "IndividualResults", backref="result", lazy="dynamic")
 
-    def __init__(self,playlist,user):
-      self.playlist = playlist
-      self.user = user
+    def __init__(self, playlist, user):
+        self.playlist = playlist
+        self.user = user
 
     def __repr__(self):
-        return '<Results %r Attempt By %r>' % (self.playlist.playlistName,self.user.username)
+        return '<Results %r Attempt By %r>' % (self.playlist.playlistName, self.user.username)
+
 
 class IndividualResults(BaseAutoPrimary):
     __tablename__ = "individualResults"
-    answerArtist =db.Column(db.String(100))
-    answerSong =db.Column(db.String(100))
-    isAnswerArtistCorrect =db.Column(db.Boolean)
+    answerArtist = db.Column(db.String(100))
+    answerSong = db.Column(db.String(100))
+    isAnswerArtistCorrect = db.Column(db.Boolean)
     isAnswerSongCorrect = db.Column(db.Boolean)
 
-    #Foreign Keys
+    # Foreign Keys
     resultId = db.Column(db.Integer, db.ForeignKey("results.id"))
     songId = db.Column(db.Integer, db.ForeignKey("song.id"))
 
-    def __init__(self, answerArtist, answerSong, isAnswerArtistCorrect, isAnswerSongCorrect,result,song):
+    def __init__(self, answerArtist, answerSong, isAnswerArtistCorrect, isAnswerSongCorrect, result, song):
         self.answerArtist = answerArtist
         self.answerSong = answerSong
         self.isAnswerArtistCorrect = isAnswerArtistCorrect
@@ -156,6 +187,4 @@ class IndividualResults(BaseAutoPrimary):
 
     def __repr__(self):
         return '<IndividualResults %r By %r >' % (self.song.songName, self.result.user.username)
-
-
 
