19938cfcd26241c5b4e06fe6bd33826ac7cc90f5
frinzekt <frinzekt@gmail.com>
2020-05-14 11:29:47 +0800

    ctrl prefix for shell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app import app, db
+from app import app, db, controllers
 # importing app variable from the app folder
 
 from app.models import *
@@ -7,4 +7,4 @@ from app.controllers import *
 #Shell Configurations
 @app.shell_context_processor
 def make_shell_contect():
-    return {"db":db,"User":User,"Privileges":Privileges,"Song":Song,"Playlist_Song":Playlist_Song,"Playlist":Playlist,"Results":Results,"IndividualResults":IndividualResults}
+    return {"ctrl":controllers,"db":db,"User":User,"Privileges":Privileges,"Song":Song,"Playlist_Song":Playlist_Song,"Playlist":Playlist,"Results":Results,"IndividualResults":IndividualResults}
